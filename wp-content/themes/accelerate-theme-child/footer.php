eb4638bf5fb42ea1f7a26365eb3211bd6e7a4617
SE Loftus <seloftus.nb@gmail.com>
2016-11-03 16:12:04 -0300

    Recommitting updated footer w/ social media icons

--- wp-content/themes/accelerate-theme-child/footer.php
+++ wp-content/themes/accelerate-theme-child/footer.php
@@ -10,24 +10,20 @@
  */
 ?>
 
-		</div><!-- #main -->
-
-
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="site-info">
-				<div class="site-description">
-				<p><?php bloginfo('description'); ?></p>
-				<p>&copy; <?php bloginfo('title'); ?>, LLC
-				</div>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		<div class="site-description">
+			<p><?php bloginfo('description'); ?></p>
+			<p>&copy; <?php bloginfo('title'); ?>, LLC
+		</div>
 				
-			<nav class="social-media-navigation" role="navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
-			</nav>
+		<nav class="social-media-navigation" role="navigation" id="navmenu"><!--added id="navmenu" trying to remove ul-->
+			<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
+		</nav>
 
 				
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 
 	<?php wp_footer(); ?>
 </body>
